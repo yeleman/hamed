494c5f5239e53bf97adbb88d822d549984f480e9
renaud gaudin <rgaudin@gmail.com>
2017-03-08 09:00:51 +0000

    STATIC_ROOT to settings

--- hamed/settings.py
+++ hamed/settings.py
@@ -177,7 +177,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
 ]
 
 COLLECT_DOCUMENTS_FOLDER = os.path.join(BASE_DIR, "Collectes-RAMED")
